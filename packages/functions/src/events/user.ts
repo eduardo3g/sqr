a3f2d3201ba4f7c5819c56f440a7f36e5af29141
eduardo3g <eduardosbrasil10@gmail.com>
2023-03-18 22:39:37 -0300

    feat: initiate Spotify client on event bus subscriber

--- packages/functions/src/events/user.ts
+++ packages/functions/src/events/user.ts
@@ -1,5 +1,18 @@
 import { Bus } from "@sqr/core/bus";
+import { Spotify } from "@sqr/core/spotify";
+import { User } from "@sqr/core/user";
 
-export const created = Bus.subscribe("user.login", async (evt) => {
-  console.log(evt);
+export const login = Bus.subscribe("user.login", async (evt) => {
+  const user = await User.fromID(evt.userID);
+  if (!user)
+    throw new Error(`User [${evt.userID}] does not exist`);
+
+  const { accessToken, refreshToken } = user;
+
+  const client = await Spotify.client({
+    access: accessToken,
+    refresh: refreshToken
+  });
+
+  await Spotify.sync(client);
 });
